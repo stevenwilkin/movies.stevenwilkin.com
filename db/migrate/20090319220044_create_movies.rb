0a773a48f6452bcbd2def949e4b7b4a4c68c40b0
Steven Wilkin <stevenwilkin@gmail.com>
2009-03-19 22:07:22 +0000

    scaffolding for movies table

---
+++ db/migrate/20090319220044_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration
+  def self.up
+    create_table :movies do |t|
+      t.string :title
+    end
+  end
+
+  def self.down
+    drop_table :movies
+  end
+end
