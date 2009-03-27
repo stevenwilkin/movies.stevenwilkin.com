e12267cb85a6d9eade5f2f32d0741b2991de5c2b
Steven Wilkin <stevenwilkin@gmail.com>
2009-03-27 19:59:15 +0000

    all movies grouped by the letter they begin with

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+
+  # a hash of letters => arrays of movies beginning with that leter
+  def self.all_grouped_by_first_letter
+    @movies = {}
+    self.all(:order => 'title ASC').each do |movie|
+      (@movies[movie.title.first.downcase] ||= []).push(movie)
+    end
+    @movies
+  end
+
 end
