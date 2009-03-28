ed845f2c1b9819520a5a7e0fae1ec14bc8b7fa55
Steven Wilkin <stevenwilkin@gmail.com>
2009-03-28 23:44:50 +0000

    movie sources

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,15 @@
 class Movie < ActiveRecord::Base
 
+  # the source marerial of the DVD, assuming pirated ;)
+  # http://www.vcdq.com/index.php?page=faq
+  SOURCES = %w(DVD-Rip R5 R5-Line DVD-Scr VHS-Scr)
+
+  # title must be present and unique
   validates_presence_of :title
   validates_uniqueness_of :title
+
+  # source must correspond to an entry in SOURCES
+  validates_inclusion_of :source, :in => 0..SOURCES.length-1
 
   # a hash of letters => arrays of movies beginning with that leter
   def self.all_grouped_by_first_letter
